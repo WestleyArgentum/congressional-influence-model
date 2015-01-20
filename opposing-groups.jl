24be442349435a6e8b116ed1ae041df6f82dd39b
WestleyArgentum <WestleyArgentum@gmail.com>
2015-01-20 18:37:36 -0500

    record aids when looking at votes, better display of output

--- opposing-groups.jl
+++ opposing-groups.jl
@@ -22,7 +22,7 @@ for (aid, bill) in bills
 
             get!(opposing_groups, pair, {
                 "total" => 0,
-                "vote_favors" => { s => 0, o => 0 },
+                "vote_favors" => { s => String[], o => String[] },
                 "oppose" => { s => 0, o => 0 },
                 "support" => { s => 0, o => 0 }
             })
@@ -33,9 +33,9 @@ for (aid, bill) in bills
             stats["oppose"][o] += 1
 
             if bill["passed"]
-                stats["vote_favors"][s] += 1
+                push!(stats["vote_favors"][s], aid)
             else
-                stats["vote_favors"][o] += 1
+                push!(stats["vote_favors"][o], aid)
             end
 
         end
@@ -48,5 +48,5 @@ sort!(biggest_adversaries, lt = (lhs, rhs)->lhs[2]["total"] > rhs[2]["total"])
 for adversaries in biggest_adversaries
     group1 = industries[adversaries[1][1]]["Catname"]
     group2 = industries[adversaries[1][2]]["Catname"]
-    println("$group1 | $group2  ($(adversaries[2]))")
+    println("$group1 | $group2  -- total: $(adversaries[2]["total"]), support: $(adversaries[2]["support"])")
 end
