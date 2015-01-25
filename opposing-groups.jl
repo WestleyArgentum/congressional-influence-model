c6f3db84107fd9eb64b7ec1e478802cfe2f31fd7
WestleyArgentum <WestleyArgentum@gmail.com>
2015-01-24 22:41:33 -0500

    wrap things up as functions better, improve pretty printing

--- opposing-groups.jl
+++ opposing-groups.jl
@@ -76,67 +76,79 @@ function money_info(money_data, code1, code2)
     return victories, average
 end
 
+function compute_opposing_groups(bills)
+    opposing_groups = Dict()
+
+    for (aid, bill) in bills
+        println( ">> $(bill["session"])-$(bill["prefix"])-$(bill["num"])-$(bill["actionId"])")
+
+        positions = bill["positions"]
+        supporters = positions["support"]
+        opposers = positions["oppose"]
+
+        contributions = load_contributions(bill, "./data/15-1-23-contributions-113")
+
+        for s in supporters
+            for o in opposers
+                pair = s < o ? (s, o) : (o, s)
+
+                stats = get!(opposing_groups, pair, {
+                    "total" => 0,
+                    "vote_favors" => { s => String[], o => String[] },
+                    "money" => Dict(),
+                    "total_money_favors" => { s => String[], o => String[] },
+                    "supported_by" => { s => String[], o => String[] },
+                })
+
+                stats["total"] += 1
+                push!(stats["supported_by"][s], aid)
+
+                stats["money"][aid] = { s => get(contributions, s, 0), o => get(contributions, o, 0) }
+
+                money_for = bill["money"]["totalFor"]
+                money_against = bill["money"]["totalAgainst"]
+
+                favored = money_for >= money_against ? stats["total_money_favors"][s] : stats["total_money_favors"][o]
+                push!(favored, aid)
+
+                favored = bill["passed"] ? stats["vote_favors"][s] : stats["vote_favors"][o]
+                push!(favored, aid)
+
+            end
+        end
+    end
+
+    for (pair, data) in opposing_groups
+        data["money_averages"] = money_info(data["money"], pair[1], pair[2])[2]
+    end
+
+    opposing_groups
+end
+
+function show_opposing_groups(opposing_groups::Dict, industries)
+    biggest_adversaries = collect(opposing_groups)
+    sort!(biggest_adversaries, lt = (lhs, rhs)->lhs[2]["total"] > rhs[2]["total"])
+
+    for adversaries in biggest_adversaries[1:20]
+        id1 = adversaries[1][1]
+        id2 = adversaries[1][2]
+        name1 = industries[id1]["Catname"]
+        name2 = industries[id2]["Catname"]
+
+        vote_favors = { id => length(votes) for (id, votes) in adversaries[2]["vote_favors"] }
+        money_averages = adversaries[2]["money_averages"]
+        total_money_favors = { id => length(bills) for (id, bills) in adversaries[2]["total_money_favors"] }
+        supported_by = { id => length(bills) for (id, bills) in adversaries[2]["supported_by"] }
+        println("$name1 ($id1) | $name2 ($id2) >> total: $(adversaries[2]["total"]), supported_by: $supported_by, vote_favors: $vote_favors, money_averages: $money_averages")
+    end
+end
+
 bills = JSON.parse(readall("./data/113th-bills.json"))
 industries = JSON.parse(readall("./data/crp-categories.json"))
 
 bills = filter_has_votes(bills)
 bills = filter_overlapping_votes(bills)
 
-opposing_groups = Dict()
-
-for (aid, bill) in bills
-    positions = bill["positions"]
-    supporters = positions["support"]
-    opposers = positions["oppose"]
-
-    contributions = load_contributions(bill, "./data/15-1-23-contributions-113")
-
-    for s in supporters
-        for o in opposers
-            pair = s < o ? (s, o) : (o, s)
-
-            stats = get!(opposing_groups, pair, {
-                "total" => 0,
-                "vote_favors" => { s => String[], o => String[] },
-                "money" => Dict(),
-                "total_money_favors" => { s => String[], o => String[] },
-                "supported_by" => { s => String[], o => String[] },
-            })
-
-            stats["total"] += 1
-            push!(stats["supported_by"][s], aid)
-
-            stats["money"][aid] = { s => get(contributions, s, 0), o => get(contributions, o, 0) }
-
-            money_for = bill["money"]["totalFor"]
-            money_against = bill["money"]["totalAgainst"]
-
-            favored = money_for >= money_against ? stats["total_money_favors"][s] : stats["total_money_favors"][o]
-            push!(favored, aid)
-
-            favored = bill["passed"] ? stats["vote_favors"][s] : stats["vote_favors"][o]
-            push!(favored, aid)
-
-        end
-    end
-end
-
-for (pair, data) in opposing_groups
-    data["money_averages"] = money_info(data["money"], pair[1], pair[2])[2]
-end
-
-biggest_adversaries = collect(opposing_groups)
-sort!(biggest_adversaries, lt = (lhs, rhs)->lhs[2]["total"] > rhs[2]["total"])
-
-for adversaries in biggest_adversaries[1:20]
-    id1 = adversaries[1][1]
-    id2 = adversaries[1][2]
-    name1 = industries[id1]["Catname"]
-    name2 = industries[id2]["Catname"]
-
-    vote_favors = { id => length(votes) for (id, votes) in adversaries[2]["vote_favors"] }
-    money_averages = adversaries[2]["money_averages"]
-    total_money_favors = { id => length(bills) for (id, bills) in adversaries[2]["total_money_favors"] }
-    supported_by = { id => length(bills) for (id, bills) in adversaries[2]["supported_by"] }
-    println("$name1 ($id1) | $name2 ($id2) >> total: $(adversaries[2]["total"]), supported_by: $supported_by, vote_favors: $vote_favors, money_averages: $money_averages")
-end
+opposing_groups = compute_opposing_groups(bills)
+println("\n-------\n")
+show_opposing_groups(opposing_groups, industries)
